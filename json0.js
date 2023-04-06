c42ada13079230b4b1f376457f0662dbe038f6c8
Mrk-003 <73594928+Mrk-003@users.noreply.github.com>
2023-04-06 14:33:42 +0800

    Add files via upload

---
+++ json0.js
@@ -0,0 +1,105 @@
+function getFieldName(fields, id) {
+  for (field of fields) {
+      if (field['id'] == id) {
+          return field['info']['label']
+      }
+  }
+}
+
+let json =`
+{
+  "fields": [
+      {
+          "id": "sitename",
+          "type": "text",
+          "info": {
+              "label": "固定桿名稱"
+          }
+      },
+      {
+          "id": "county",
+          "type": "text",
+          "info": {
+              "label": "縣市"
+          }
+      },
+      {
+          "id": "aqi",
+          "type": "text",
+          "info": {
+              "label": "限速標準"
+          }
+      },
+      {
+          "id": "pollutant",
+          "type": "text",
+          "info": {
+              "label": "空氣污染指標物"
+          }
+      },
+      {
+          "id": "status",
+          "type": "text",
+          "info": {
+              "label": "位址"
+          }
+      },
+      {
+          "id": "longitude",
+          "type": "text",
+          "info": {
+              "label": "經度"
+          }
+      },
+      {
+          "id": "latitude",
+          "type": "text",
+          "info": {
+              "label": "緯度"
+          }
+      },
+      {
+          "id": "siteid",
+          "type": "text",
+          "info": {
+              "label": "測站編號"
+          }
+      }
+  ],
+  "resource_id": "8d2f907f-bbb4-4fdf-8f08-8eabae15da45",
+  "__extras": {
+      "api_key": "ffc6ebbb-b91e-47d5-bf6a-794f291b5f2d"
+  },
+  "include_total": true,
+  "total": "85",
+  "resource_format": "object",
+  "limit": "500",
+  "offset": "0",
+  "_links": {
+      "start": "\/api\/v2\/aqx_p_432?api_key=ffc6ebbb-b91e-47d5-bf6a-794f291b5f2d&limit=500&format=json",
+      "next": "\/api\/v2\/aqx_p_432?api_key=ffc6ebbb-b91e-47d5-bf6a-794f291b5f2d&limit=500&format=json&offset=500"
+  },
+  "records": [
+      {
+          "sitename": "湖口",
+          "county": "新竹",
+          "aqi": "測速照相速限60公里",
+          "pollutant": "",
+          "status": "湖口鄉台一線南下車道61K南下",
+          "longitude": "121.760056",
+          "latitude": "25.129167",
+          "siteid": "1"
+      },
+      {
+        "sitename": "竹北",
+        "county": "新竹",
+        "aqi": "測速照相速限70公里",
+        "pollutant": "",
+        "status": "西濱公路台15線69K鳳鼻隧道南端出口",
+        "longitude": "121.764056",
+        "latitude": "25.127167",
+        "siteid": "1"
+    }
+  ]
+}
+`
